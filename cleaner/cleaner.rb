f6a908e51c613f4babf3c8954f2e9d8f815d50b6
Daniel Au <dakmau@gmail.com>
2019-02-19 19:41:13 -0800

    Update data

--- cleaner/cleaner.rb
+++ cleaner/cleaner.rb
@@ -7,41 +7,35 @@ class Category
   def initialize(category)
     @category = category
     @names = []
+  end
 
+  def call
     get_names
+    match_names
   end
 
   def get_names
     j = JSON.parse(File.read("./names_item_s.json"))
     j.each do |entry|
-      names << hash["name"]
+      names << entry["name"]
     end
+  end
+
+  def match_names
+    matcher = FuzzyMatch.new(@names)
+    binding.pry
+    Dir.entries("./raw_data/100%").select {|f| !File.directory? f}.each do |dir|
+      name = File.read("./raw_data/100%/#{dir}/name.txt").strip
+      unless @names.include? name
+        result = matcher.find_best(name)
+        puts "#{name}, best was #{result}"
+      end
+      # prices = File.read("./raw_data/chairs/#{dir}/data.txt").scan(/of (\d*.*) m/).map{|arr| arr.first}.map{|h| h.gsub(/,/, "").to_i }
+      # json << {"#{name}": prices}
+    end
+    
+    # File.write("scroll_prices.json", JSON.pretty_generate(json))
   end
 end
 
-# f = File.read("./names_item_s.json")
-# chair_json = JSON.parse(f)
-
-
-# chair_names = []
-
-# chair_json.each do |hash|
-#   chair_names << hash["name"]
-# end
-
-binding.pry
-
-matcher = FuzzyMatch.new(chair_names)
-
-json = []
-
-Dir.entries("./raw_data/chairs").select {|f| !File.directory? f}.each do |dir|
-  name = File.read("./raw_data/chairs/#{dir}/name.txt").strip
-  prices = File.read("./raw_data/chairs/#{dir}/data.txt").scan(/of (\d*.*) m/).map{|arr| arr.first}.map{|h| h.gsub(/,/, "").to_i }
-  json << {"#{name}": prices}
-end
-
-File.write("scroll_prices.json", JSON.pretty_generate(json))
-
-binding.pry
-puts "lol"
+c = Category.new("scrolls").call
